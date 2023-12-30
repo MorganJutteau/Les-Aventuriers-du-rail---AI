518396b81dd5325f446329e5a45c10dd1e01d0dc
Morgan Jutteau <morgan-mj@hotmail.fr>
2023-12-30 10:23:33 -0500

    finalisation du constructeur de TransportCardDeck

--- deck/transport_card_deck.py
+++ deck/transport_card_deck.py
@@ -1,5 +1,24 @@
 from deck.deck import Deck
+from card.transport_card import TransportCard
 
 class TransportCardDeck(Deck):
     def __init__(self):
         super().__init__()
+
+        # chargement des cartes transport classique
+        for i in range(12):
+            self.add_card(TransportCard(color = "blanc"))
+            self.add_card(TransportCard(color = "bleu"))
+            self.add_card(TransportCard(color = "jaune"))
+            self.add_card(TransportCard(color = "vert"))
+            self.add_card(TransportCard(color = "rouge"))
+            self.add_card(TransportCard(color = "violet"))
+            self.add_card(TransportCard(color = "noir"))
+            self.add_card(TransportCard(color = "marron"))
+        
+        # chargement des cartes locomotive
+        for i in range(14):
+            self.add_card(TransportCard(color = "locomotive"))
+        
+        # mélange des cartes
+        self.shuffle(self.cards)
