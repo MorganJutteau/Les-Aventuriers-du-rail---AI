06e4f3ede619f8f3aef4e5335bd1e6c682357e33
Morgan Jutteau <morgan-mj@hotmail.fr>
2023-12-30 06:39:41 -0500

    Ajout des classes de deck de transport/destination

---
+++ deck/destination_card_deck.py
@@ -0,0 +1,5 @@
+from deck.deck import Deck
+
+class DestinationCardDeck(Deck):
+    def __init__(self):
+        super().__init__()
---
+++ deck/transport_card_deck.py
@@ -0,0 +1,5 @@
+from deck.deck import Deck
+
+class TransportCardDeck(Deck):
+    def __init__(self):
+        super().__init__()
