1285b64a0466fff5fcca5d23c6505d88fad2138e
Morgan Jutteau <morgan-mj@hotmail.fr>
2023-12-30 10:16:29 -0500

    ajout de quelques méthodes pour la classe Deck

---
+++ deck/deck.py
@@ -0,0 +1,14 @@
+from card.card import Card
+from random import shuffle
+from typing import Deque
+
+class Deck:
+    def __init__(self):
+        self.cards : Deque[Card] = ()
+    def shuffle(self):
+        shuffle(self.cards)
+    def draw(self):
+        return self.cards.popleft()
+    def add_card(self, card : Card):
+        self.cards.append(card)
+        
